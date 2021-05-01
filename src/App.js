367eb16787f5b112129bcdb71669764b1b22e26b
your.user.name <56067032+tripathiadarsh@users.noreply.github.com>
2021-05-01 21:35:46 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,56 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import useFetchJobs from './useFetchJobs'
+import { Container, Spinner } from 'react-bootstrap'
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import Job from './Job'
+import JobsPagination from './JobsPagination';
+import SearchForm from './SearchForm';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const [params, setParams] = useState({})
+  const [page, setPage] = useState(1)
+  const { jobs, loading, error, hasNextPage } = useFetchJobs(params, page)
+    const footer= {
+      position: "fixed",
+      left: "0",
+      right: "0",
+      bottom: "0",
+      width: "100%",
+      backgroundColor: "white",
+      color:"black",
+      fontWeight: "bold",
+      textAlign: "center",
+  };
+ function handleParamChange(e) {
+    const param = e.target.name
+    const value = e.target.value
+    setPage(1)
+    setParams(prevParams => {
+      return { ...prevParams, [param]: value }
+    })
+  };
+
+return (                                                                                                                                                                                       
+    <Container className="my-4 ">
+
+      <h1 className="mb-4 text-white "><span role="img" aria-label="">🤙</span>GitHub Jobs Search Made Easy with job search web app<span role="img" aria-label="">🤙</span></h1>
+      <SearchForm params={params} onParamChange={handleParamChange} />
+      <JobsPagination page={page} setPage={setPage} hasNextPage={hasNextPage} />
+      {loading && <Spinner className="mb-3 mt-2" id="spinner " animation="grow" style={{color:"white"}} role="status">
+  <span className="sr-only ml-5">Loading...</span>
+</Spinner>} 
+      {error && <h3 className="text-white mb-4 ">Error. Try Refreshing your page, Check your internet connection. <span role="img" aria-label="" > 👈</span> </h3>}
+      {jobs.map(job => {
+        return <Job key={job.id} job={job} />
+      })}
+      <JobsPagination page={page} setPage={setPage} hasNextPage={hasNextPage} />
+      <div style={footer}>
+        <p>Develop and design by Adarsh Tripathi<span role="img" aria-label=""> 👇</span></p>
+        <p>My Github Profile link <span role="img" aria-label=""> 👉</span>
+         <a target="blank"href="https://github.com/adarshtiwari1998">https://github.com/adarshtiwari1998</a> </p>
+        </div>
+    </Container>
+  )
 }
 
 export default App;
---
+++ src/Job.js
@@ -0,0 +1,51 @@
+import React, { useState } from 'react'
+import { Card, Badge, Button, Collapse } from 'react-bootstrap'
+import ReactMarkdown from 'react-markdown'
+
+export default function Job({ job }) {
+  const [open, setOpen] = useState(false)
+  var well={
+    boxShadow: " 0 3px 10px rgba(60, 72, 88, 0.15)",
+    borderradius:"30px"
+}
+const avatarImage = {
+  boxShadow: "0 .125rem .25rem  #9E9E9E",
+  borderRadius:"20px"
+};
+
+  return (
+    <Card className="mb-3">
+      <Card.Body style={well}>
+        <div className="d-flex justify-content-between">
+          <div>
+            <Card.Title as="h4" >
+              {job.title} - <span className="text-muted font-weight-light">{job.company}</span>
+            </Card.Title>
+            <Card.Subtitle className="text-muted mb-2">
+              {new Date(job.created_at).toLocaleDateString()}
+            </Card.Subtitle>
+            <Badge variant="secondary" className="mr-2">{job.type}</Badge>
+            <Badge variant="secondary">{job.location}</Badge>
+            <div style={{ wordBreak: 'break-all' }}>
+              <ReactMarkdown source={job.how_to_apply} />
+            </div>
+          </div>
+          <img className="d-none d-md-block "border="primary" style={avatarImage} height="50" alt={job.company} src={job.company_logo} />
+        </div>
+        <Card.Text>
+          <Button
+            onClick={() => setOpen(prevOpen => !prevOpen)}
+            variant="danger"
+          >
+            {open ? 'Hide Details' : 'View Details'}
+          </Button>
+        </Card.Text>
+        <Collapse in={open}>
+          <div className="mt-4">
+            <ReactMarkdown source={job.description} />
+          </div>
+        </Collapse>
+      </Card.Body>
+    </Card>
+  )
+}
